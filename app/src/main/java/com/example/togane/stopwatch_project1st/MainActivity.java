84c3b68fd827168c7b91c441cf017feceb4be339
當金將見 <togane@toukin-no-MacBook-Air-10.local>
2015-10-27 17:58:02 +0900

    test

--- app/src/main/java/com/example/togane/stopwatch_project1st/MainActivity.java
+++ app/src/main/java/com/example/togane/stopwatch_project1st/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -13,6 +15,16 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //スタートストップボタン切り替え
+    public void star_stop(View view){
+
+        TextView tv = (TextView) findViewById(R.id.start);
+
+        tv.setText("Stop");
+    }
+
+
 
 
     @Override
